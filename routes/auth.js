23fe230b794117cfb53a50a9c0eb23ecc7fbbf54
veronicadev <viarengoveronica@gmail.com>
2019-11-08 15:14:30 +0100

    user model

---
+++ routes/auth.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const { body, check } = require('express-validator/check');
+const router = express.Router();
+
+//router.post('/signup', authController.postSignup);
+module.exports = router;
